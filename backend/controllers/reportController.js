38a6a86e22f1d9d87f08faa07ae987fb186fd27e
alvamau5 <lucioaalvaradomolina@outlook.com>
2026-09-23 13:43:47 -0600

    feat: Se crean actualiza la funcion updateReport y crea funcion deleteReport y se crean sus respectivas rutas.

--- backend/controllers/reportController.js
+++ backend/controllers/reportController.js
@@ -43,9 +43,22 @@ const updateReport = async (req, res) => {
     if (!report) {
       return res.status(404).send({ msg: "Report not found" });
     }
+    res.status(200).json(report);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-export { createReports, getReports, updateReport, getReportById };
+const deleteReport = async (req, res) => {
+  try {
+    const report = await Report.findByIdAndDelete(req.params.id);
+    if (!report) {
+      return res.status(404).send({ msg: "Report not found" });
+    }
+    res.status(200).json({ msg: "Report deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { createReports, getReports, updateReport, getReportById, deleteReport };
--- backend/routes/reportRoutes.js
+++ backend/routes/reportRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createReports, getReports } from "../controllers/reportController.js";
+import {
+  createReports,
+  getReports,
+  updateReport,
+  getReportById,
+  deleteReport,
+} from "../controllers/reportController.js";
 
 const router = express.Router();
 
 router.post("/", createReports);
 router.get("/", getReports);
+router.get("/:id", getReportById);
+router.put("/:id", updateReport);
+router.delete("/:id", deleteReport);
 
 export default router;
